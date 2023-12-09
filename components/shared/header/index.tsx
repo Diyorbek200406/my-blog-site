d5716633758c69f7cd45bcd58f08cc4e0b9e21c1
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-12-10 00:51:53 +0500

    header dropdown

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,60 +1,84 @@
 "use client";
 
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { AlignLeftIcon, Menu, X } from "lucide-react";
-import { ModeToggle } from "../mode";
 import { navigation } from "@/lib/constants";
-import { usePathname } from "next/navigation";
+import { ModeToggle } from "../mode";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
 
 const Header = () => {
-  const [toggle, setToggle] = useState<boolean>(false);
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const pathname = usePathname();
-  const toggleNavbar = () => setToggle(!toggle);
+  const toggleNavbar = () => setIsNavbarOpen(!isNavbarOpen);
+
+  const renderNavLinks = () => (
+    <nav
+      className={cn(
+        "hidden z-[99] md:flex items-center gap-x-8 transition-opacity",
+        isNavbarOpen ? "opacity-0" : "opacity-100"
+      )}
+    >
+      {navigation.map((item, idx) => (
+        <Link
+          key={idx}
+          href={item.route}
+          className={cn("text-sm font-bold hover:underline", item.route === pathname && "underline")}
+        >
+          {item.title}
+        </Link>
+      ))}
+    </nav>
+  );
+
+  const renderMobileNavLinks = () => (
+    <nav
+      className={cn(
+        "md:hidden z-[99] w-screen h-screen fixed top-0 bg-background transition-transform duration-700 delay-75",
+        isNavbarOpen ? "translate-x-0" : "translate-x-full"
+      )}
+    >
+      <div className="flex items-center justify-end p-4 bg-transparent">{closeIcon()}</div>
+
+      {navigation.map((item, idx) => (
+        <Link
+          key={idx}
+          href={item.route}
+          onClick={toggleNavbar}
+          className={cn(
+            "w-full block text-xl font-bold p-4 text-center hover:bg-slate-900",
+            item.route === pathname && "bg-slate-900"
+          )}
+        >
+          {item.title}
+        </Link>
+      ))}
+    </nav>
+  );
+
+  const closeIcon = () => <X className="md:hidden cursor-pointer" onClick={toggleNavbar} />;
+  const openIcon = () => <Menu className="md:hidden cursor-pointer" onClick={toggleNavbar} />;
+  const logo = () => (
+    <Link href="/" className="flex items-center gap-2">
+      <AlignLeftIcon /> Diyorbek
+    </Link>
+  );
 
   return (
-    <div className="relative z-[999]">
-      <div className="w-full h-[10vh] fixed top-0 left-0 right-0 flex items-center justify-between px-10 py-5 bg-background z-50">
-        <Link href="/" className="flex items-center gap-2">
-          <AlignLeftIcon />
-          Diyorbek
-        </Link>
+    <>
+      <div className="w-full h-[10vh] fixed top-0 flex items-center justify-between px-10 py-5 bg-background z-[99]">
+        {logo()}
 
         <div className="flex items-center gap-x-8">
-          <nav className="hidden md:flex items-center gap-x-8 ">
-            {navigation.map((item, idx) => (
-              <Link key={idx} href={item.route} className={cn("text-sm font-bold hover:underline", item.route === pathname && "underline")}>
-                {item.title}
-              </Link>
-            ))}
-          </nav>
-
+          {renderNavLinks()}
           <ModeToggle />
-
-          {toggle ? (
-            <X className="md:hidden cursor-pointer" onClick={toggleNavbar} />
-          ) : (
-            <Menu className="md:hidden cursor-pointer" onClick={toggleNavbar} />
-          )}
+          {!isNavbarOpen && openIcon()}
         </div>
       </div>
 
-      {toggle && (
-        <nav className={cn("md:hidden w-screen h-screen fixed top-[10vh] bg-background")}>
-          {navigation.map((item, idx) => (
-            <Link
-              key={idx}
-              href={item.route}
-              onClick={toggleNavbar}
-              className={cn("w-full block p-4 text-center hover:bg-slate-900 bg-slate-800", item.route === pathname && "bg-slate-900")}
-            >
-              {item.title}
-            </Link>
-          ))}
-        </nav>
-      )}
-    </div>
+      {renderMobileNavLinks()}
+    </>
   );
 };
 

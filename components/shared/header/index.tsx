1772d478cf8af10521f72285738c41002a72cac6
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-12-09 22:29:45 +0500

    toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import Link from "next/link";
-import { AlignLeftIcon } from "lucide-react";
+import { AlignLeftIcon, Menu } from "lucide-react";
 import { ModeToggle } from "../mode";
 import { navigation } from "@/lib/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
   const pathname = usePathname();
 
   return (
@@ -17,15 +20,34 @@ const Header = () => {
         Diyorbek
       </Link>
 
-      <nav className="flex items-center gap-x-8">
-        {navigation.map((item, idx) => (
-          <Link key={idx} href={item.route} className={cn("text-sm font-bold hover:underline", item.route === pathname && "underline")}>
-            {item.title}
-          </Link>
-        ))}
+      <div className="flex items-center gap-x-8">
+        <nav className="hidden items-center gap-x-8 md:flex">
+          {navigation.map((item, idx) => (
+            <Link key={idx} href={item.route} className={cn("text-sm font-bold hover:underline", item.route === pathname && "underline")}>
+              {item.title}
+            </Link>
+          ))}
+        </nav>
 
         <ModeToggle />
-      </nav>
+
+        <Menu className="block md:hidden cursor-pointer" onClick={() => setToggle((prev) => !prev)} />
+
+        <nav
+          className={cn("flex flex-col gap-4 absolute top-0 right-0 w-screen h-screen bg-slate-700 transition-all duration-700", toggle && "hidden transition-all duration-700")}
+        >
+          {navigation.map((item, idx) => (
+            <Link
+              key={idx}
+              onClick={() => setToggle((prev) => !prev)}
+              href={item.route}
+              className={cn("text-xl font-medium hover:underline w-full", item.route === pathname && "bg-slate-500")}
+            >
+              <Button className="w-full">{item.title}</Button>
+            </Link>
+          ))}
+        </nav>
+      </div>
     </div>
   );
 };

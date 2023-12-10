b879d715cb08bda672fd665ded0e387db52a243b
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-12-10 10:14:42 +0500

    responsive projects

--- app/(root)/projects/page.tsx
+++ app/(root)/projects/page.tsx
@@ -38,14 +38,23 @@ const Projects = () => {
         </Link>
       </motion.div>
 
-      <main className="grid grid-cols-1 md:grid-col-3 gap-8">
+      <main className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8">
         {myProjects.map((project, idx) => (
           <div key={idx} className="max-w-sm rounded overflow-hidden shadow-lg">
-            <Image priority className="w-full h-56" src={project.image} alt="Sunset in the mountains" width={100} height={100} />
+            <Image
+              priority
+              className="w-full h-56"
+              src={project.image}
+              alt="Sunset in the mountains"
+              width={100}
+              height={100}
+            />
 
             <div className="p-4">
               <h2 className="font-bold text-xl">{project.name}</h2>
-              <p className="text-slate-500 py-4">Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+              <p className="text-slate-500 py-4">
+                Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+              </p>
 
               <Link href={project.route} target="_blank">
                 <Button className="flex items-center justify-center gap-4">

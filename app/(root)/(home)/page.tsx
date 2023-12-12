1dc64095ec7c95735dc40e393603020ebad5c389
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-12-12 13:23:05 +0500

    some

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,7 +10,12 @@ import ReactTypingEffect from "react-typing-effect";
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <motion.div initial={{ scale: 0, opacity: 0, x: -500 }} animate={{ scale: 1, opacity: 1, x: 0 }} exit={{ scale: 0, opacity: 0, x: 500 }} transition={{ duration: 1, ease: "easeInOut" }}>
+      <motion.div
+        initial={{ scale: 0, opacity: 0, x: -500 }}
+        animate={{ scale: 1, opacity: 1, x: 0 }}
+        exit={{ scale: 0, opacity: 0, x: 500 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
         <Image priority className="rounded rounded-t-xl object-cover" src={MyImage} width={250} alt="Image" />
       </motion.div>
 
@@ -21,7 +26,7 @@ const Home = () => {
         transition={{ duration: 1, delay: 0.1, ease: "easeInOut" }}
       >
         <h1>
-          <ReactTypingEffect cursor=" " text={["Erkinov Diyorbek"]} />
+          <ReactTypingEffect cursor=" " text={["Erkinov Diyorbek", "Erkinov", "Diyorbek"]} />
         </h1>
       </motion.div>
 
@@ -32,7 +37,10 @@ const Home = () => {
         transition={{ duration: 1, delay: 0.2, ease: "easeInOut" }}
       >
         <div>
-          <ReactTypingEffect cursor=" " text={["I'm a Frontend Developer", "I'm a Backend Developer", "I'm a Full Stack Developer"]} />
+          <ReactTypingEffect
+            cursor=" "
+            text={["I'm a Frontend Developer", "I'm a Backend Developer", "I'm a Full Stack Developer"]}
+          />
         </div>
       </motion.div>
 

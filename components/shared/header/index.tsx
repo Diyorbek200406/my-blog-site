0da6d4801ee825cd18f9ca3495c6c5e77a3d2a0d
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-12-12 15:10:06 +0500

    favicon ico

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -14,9 +14,21 @@ const Header = () => {
   const toggleNavbar = () => setIsNavbarOpen(!isNavbarOpen);
 
   const renderNavLinks = () => (
-    <nav className={cn("hidden z-[9] md:flex items-center gap-x-8 transition-opacity", isNavbarOpen ? "opacity-0" : "opacity-100")}>
+    <nav
+      className={cn(
+        "hidden z-[9] md:flex items-center gap-x-8 transition-opacity",
+        isNavbarOpen ? "opacity-0" : "opacity-100"
+      )}
+    >
       {navigation.map((item, idx) => (
-        <Link key={idx} href={item.route} className={cn("text-sm font-bold py-2 dark:border-b-slate-700 border-slate-500 hover:border-b-2", item.route === pathname && "border-b-2")}>
+        <Link
+          key={idx}
+          href={item.route}
+          className={cn(
+            "text-sm font-bold py-2 dark:border-b-slate-700 border-slate-500 hover:border-b-2",
+            item.route === pathname && "border-b-2"
+          )}
+        >
           {item.title}
         </Link>
       ))}
@@ -24,7 +36,12 @@ const Header = () => {
   );
 
   const renderMobileNavLinks = () => (
-    <nav className={cn("md:hidden z-[9] w-screen h-screen fixed top-0 bg-background transition-transform duration-700 delay-75", isNavbarOpen ? "translate-x-0" : "translate-x-full")}>
+    <nav
+      className={cn(
+        "md:hidden z-[9] w-screen h-screen fixed top-0 bg-background transition-transform duration-700 delay-75",
+        isNavbarOpen ? "translate-x-0" : "translate-x-full"
+      )}
+    >
       <div className="flex items-center justify-end p-4 bg-transparent">{closeIcon()}</div>
 
       {navigation.map((item, idx) => (
@@ -32,7 +49,10 @@ const Header = () => {
           key={idx}
           href={item.route}
           onClick={toggleNavbar}
-          className={cn("w-full block text-xl font-bold p-4 text-center dark:hover:bg-slate-900 hover:bg-slate-400", item.route === pathname && "dark:bg-slate-900 bg-slate-300")}
+          className={cn(
+            "w-full block text-xl font-bold p-4 text-center dark:hover:bg-slate-900 hover:bg-slate-400",
+            item.route === pathname && "dark:bg-slate-900 bg-slate-300"
+          )}
         >
           {item.title}
         </Link>
